3ce53e1a303dbd0242595429827c4f7f55524321
samusesapple <gwansammy@gmail.com>
2024-12-08 19:47:12 +0900

    initial project

---
+++ Thread-clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Thread-clone
+//
+//  Created by Sam Sung on 12/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Thread-clone/Thread_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Thread_cloneApp.swift
+//  Thread-clone
+//
+//  Created by Sam Sung on 12/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Thread_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
