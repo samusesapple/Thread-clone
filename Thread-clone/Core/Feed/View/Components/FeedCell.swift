3a85cf26b4bd4440afe9bfbe8a206445cf9fef24
samusesapple <gwansammy@gmail.com>
2024-12-15 21:45:33 +0900

    fix: FeedCell UI 수정

--- Thread-clone/Core/Feed/View/Components/FeedCell.swift
+++ Thread-clone/Core/Feed/View/Components/FeedCell.swift
@@ -11,61 +11,71 @@ struct FeedCell: View {
     var post: Post?
     
     var body: some View {
-        HStack(alignment: .top) {
-            Image("sam")
-                .resizable()
-                .frame(width: 40, height: 40)
-                .clipShape(Circle())
-            
-            VStack(alignment: .leading, spacing: 6) {
-                HStack(spacing: 6) {
-                    Text(post?.userName ?? "성현관")
+        VStack {
+            HStack(alignment: .top) {
+                Image("sam")
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                
+                VStack(alignment: .leading, spacing: 6) {
+                    HStack(spacing: 6) {
+                        Text(post?.userName ?? "성현관")
+                            .font(.callout)
+                            .bold()
+                        
+                        Spacer()
+                        
+                        Text(timeAgo(from: post?.date ?? Date()))
+                            .font(.caption)
+                            .foregroundStyle(Color(.systemGray))
+                        
+                        Image(systemName: "ellipsis")
+                            .font(.system(size: 16))
+                    }
+                    
+                    Text(post?.detail ?? "게시물 내용")
                         .font(.callout)
-                        .bold()
                     
-                    Spacer()
-                    
-                    Text(timeAgo(from: post?.date ?? Date()))
-                        .font(.caption)
-                        .foregroundStyle(Color(.systemGray))
-                    
-                    Image(systemName: "ellipsis")
-                        .font(.system(size: 16))
+                    HStack(spacing: 12) {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "heart")
+                                .font(.system(size: 16))
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "bubble.right")
+                                .font(.system(size: 16))
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "arrow.rectanglepath")
+                                .font(.system(size: 16))
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "paperplane")
+                                .font(.system(size: 16))
+                            
+                        }
+                    }
+                    .foregroundStyle(.black)
+                    .padding(.vertical, 8)
                 }
-                
-                Text(post?.detail ?? "게시물 내용")
-                    .font(.callout)
-                
-                HStack(spacing: 12) {
-                    // 좋아요, 댓글, 리포스트, 전송 버튼
-                    Image(systemName: "heart")
-                        .font(.system(size: 16))
-                        .onTapGesture {
-                            print("좋아요 하기")
-                        }
-                    
-                    Image(systemName: "bubble.right")
-                        .font(.system(size: 16))
-                        .onTapGesture {
-                            print("댓글 보기")
-                        }
-                    
-                    Image(systemName: "arrow.rectanglepath")
-                        .font(.system(size: 16))
-                        .onTapGesture {
-                            print("리포스트 하기")
-                        }
-                    
-                    Image(systemName: "paperplane")
-                        .font(.system(size: 16))
-                        .onTapGesture {
-                            print("공유하기")
-                        }
-                }
-                .padding(.vertical, 12)
             }
+            .padding()
+            
+            Divider()
+                .padding(.horizontal)
         }
-        .padding(.top, 12)
     }
 }
 
